91b85915ae4d7f0e36078484c6e62fedbbbe50fa
Amen Gemeda <amentamene@gmail.com>
2021-01-08 16:48:51 +0300

    very first commit

---
+++ Js/common.js
@@ -0,0 +1,9 @@
+function backToLogin(){
+    window.location.href="login.html";
+}
+function uploadPhoto(){
+    document.getElementById('photo').click();
+}
+function login(){
+    window.location.href="homepage.html";
+}
